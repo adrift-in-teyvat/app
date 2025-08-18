c4e1751e2b40f9922c0ede819b0da721c648ff79
Shob3r <andrewpinter23@gmail.com>
2025-08-18 18:16:53 +0200

    map almost works wahoo

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,31 +1,50 @@
 import '../index.css';
 import * as PIXI from 'pixi.js';
 import { Viewport } from 'pixi-viewport';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 const maxZoomLevel = 6;
 
-const giMapTiles =
-  'https://cdn.jsdelivr.net/gh/adrift-in-teyvat/map-tiles@main';
+const giMapTiles ='https://cdn.jsdelivr.net/gh/adrift-in-teyvat/map-tiles@main';
 const zoomLevelLinks = ['10', '11', '12', '13', '14', '15'];
 const teyvatTiles = `${giMapTiles}/teyvat`;
 let currentZoom = 0;
 
+function drawMap(viewport: Viewport) {
+  const wTiles = Math.ceil(window.innerWidth / 256);
+  const hTiles = Math.ceil(window.innerHeight / 256);
+
+  for (let i = 0; i <= hTiles; i++) {
+    let x = viewport.left;
+    let y = viewport.bottom - i * 256;
+
+    for (let j = 0; j <= wTiles; j++) {
+      const url = `${teyvatTiles}/${zoomLevelLinks[currentZoom]}/${Math.ceil(x / 256)}_${-Math.ceil(y / 256)}.webp`;
+      console.log(url);
+
+      const loadImage = async (targetX: number, targetY: number) => {
+        const texture = await PIXI.Assets.load(url);
+        const sprite = new PIXI.Sprite(texture);
+        const vpSprite = viewport.addChild(sprite);
+        vpSprite.position.set(targetX, targetY);
+      };
+      loadImage(x, y); // No need to wait
+      x += 256;
+    }
+  }
+}
+
 export function Map() {
   useEffect(() => {
     const app = new PIXI.Application();
-    app
-      .init({
-        background: '#1f1e33',
+    app.init({
+        background: '#000000',
         resizeTo: document.body,
         antialias: true,
         autoDensity: true,
         resolution: window.devicePixelRatio,
-      })
-      .then(() => {
-        const container = document.getElementById(
-          'map-container'
-        ) as HTMLDivElement;
+      }).then(() => {
+        const container = document.getElementById('map-container') as HTMLDivElement;
         container.appendChild(app.canvas);
 
         const viewport = new Viewport({
@@ -33,29 +52,12 @@ export function Map() {
           screenHeight: window.innerHeight,
           events: app.renderer.events,
         });
+        viewport.addListener('drag-end', () => drawMap(viewport));
+        viewport.cullable = true;
 
         app.stage.addChild(viewport);
         viewport.drag().pinch().wheel().decelerate();
-
-        const wTiles = Math.ceil(viewport.width / 256);
-        const hTiles = Math.ceil(viewport.height / 256);
-
-        for (let i = 0; i <= hTiles; i++) {
-          let x = viewport.left; // Resets each time this loop restarts, but the nested for loop will be able to add onto it
-          let y = viewport.top + i * 256; // Placing tiles from the top down, we'd be subtracting
-
-          for (let j = 0; j <= wTiles; j++) {
-            const url = `${teyvatTiles}/${zoomLevelLinks[currentZoom]}/${Math.ceil(x / 256)}_${Math.ceil(y / 256)}.jpg`;
-            console.log(url);
-            async () => {
-              const texture = await PIXI.Assets.load(url);
-              const sprite = new PIXI.Sprite(texture);
-              const vpSprite = viewport.addChild(sprite);
-              vpSprite.position.set(x, y);
-              x += 256;
-            };
-          }
-        }
+        drawMap(viewport);
       });
   }, []);
 
