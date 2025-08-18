6777ec71d5020876d17852d1be6762e8d87af022
Shob3r <andrewpinter23@gmail.com>
2025-08-18 19:12:00 +0200

    i suck so bad :sob:

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -3,24 +3,21 @@ import * as PIXI from 'pixi.js';
 import { Viewport } from 'pixi-viewport';
 import { useEffect } from 'react';
 
-const maxZoomLevel = 6;
-
 const giMapTiles ='https://cdn.jsdelivr.net/gh/adrift-in-teyvat/map-tiles@main';
 const zoomLevelLinks = ['10', '11', '12', '13', '14', '15'];
 const teyvatTiles = `${giMapTiles}/teyvat`;
 let currentZoom = 0;
 
 function drawMap(viewport: Viewport) {
+  viewport.removeChildren();
   const wTiles = Math.ceil(window.innerWidth / 256);
   const hTiles = Math.ceil(window.innerHeight / 256);
-
   for (let i = 0; i <= hTiles; i++) {
     let x = viewport.left;
     let y = viewport.bottom - i * 256;
 
     for (let j = 0; j <= wTiles; j++) {
       const url = `${teyvatTiles}/${zoomLevelLinks[currentZoom]}/${Math.ceil(x / 256)}_${-Math.ceil(y / 256)}.webp`;
-      console.log(url);
 
       const loadImage = async (targetX: number, targetY: number) => {
         const texture = await PIXI.Assets.load(url);
@@ -53,20 +50,22 @@ export function Map() {
           events: app.renderer.events,
         });
         viewport.addListener('drag-end', () => drawMap(viewport));
+        viewport.addListener('moved', () => "")
+        viewport.addListener('zoomed-end', () => {
+          currentZoom = Math.round(viewport.scale.x / 2 );
+          drawMap(viewport);
+        })
         viewport.cullable = true;
 
         app.stage.addChild(viewport);
-        viewport.drag().pinch().wheel().decelerate();
+        viewport.drag().pinch().wheel().decelerate().setZoom(1).clampZoom({ maxScale: 12, minScale: 0});
         drawMap(viewport);
       });
   }, []);
 
   return (
     <>
-      <div
-        id="map-container"
-        className="w-screen h-screen overflow-hidden"
-      ></div>
+      <div id="map-container" className="w-screen h-screen overflow-hidden"/>
     </>
   );
 }
