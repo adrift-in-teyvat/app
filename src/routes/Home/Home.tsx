f37beec8b4bc27a49fc9d1ac4325a70f1558678a
ChakornK <120770627+ChakornK@users.noreply.github.com>
2025-08-21 18:54:04 -0700

    fix(homepage): make background non-interactable

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -4,7 +4,7 @@ import { HomePageBg, HomePageText } from "../../Components/HomePageBg";
 function Home() {
   return (
     <div className="bg-stone-700">
-      <div className="relative m-auto w-screen max-w-[calc(5/3*100vh)] h-dscreen overflow-hidden">
+      <div className="relative m-auto w-screen max-w-[calc(5/3*100vh)] h-dscreen overflow-hidden *:pointer-events-none">
         <HomePageBg />
         <HomePageText />
       </div>
