03ee7e5132e33a7ad3020f179f1eddd0c1c38a8a
ChakornK <120770627+ChakornK@users.noreply.github.com>
2025-08-23 12:50:33 -0700

    feat: Add start banner to home page

--- src/routes/home/page.tsx
+++ src/routes/home/page.tsx
@@ -1,5 +1,7 @@
 import "../../index.css";
+import "./page.css";
 import { HomePageBg, HomePageText } from "../../components/HomePageBg";
+import { motion } from "motion/react";
 import { useLocation } from "preact-iso";
 
 function Home() {
@@ -10,6 +12,9 @@ function Home() {
       <div className="relative m-auto w-screen max-w-[calc(5/3*100vh)] h-dscreen overflow-hidden *:pointer-events-none" onClick={() => router.route("/game")}>
         <HomePageBg />
         <HomePageText />
+        <motion.div className={"start-banner"} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, type: "tween", delay: 0.8 }}>
+          <p>Click anywhere to start</p>
+        </motion.div>
       </div>
     </div>
   );
